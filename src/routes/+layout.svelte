501ba810bfca32f9b89e4eebe1868c23e16c6d66
Koen Raijer <koenrayer@gmail.com>
2024-01-29 21:22:57 +0100

    Styled advice and added copy button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,13 +17,8 @@
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-2xl">ReAsset</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-
-			</svelte:fragment>
+		<AppBar gridColumns="" slotDefault="place-self-center" slotTrail="place-content-end">
+			<strong class="text-3xl mx-auto">Re<svg xmlns="http://www.w3.org/2000/svg" class="inline w-12 h-12 px-2" viewBox="0 0 24 24" width="24" height="24"><path d="M15.25 14a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5h-3.5Z"></path><path d="M1.75 3h20.5c.966 0 1.75.784 1.75 1.75v14.5A1.75 1.75 0 0 1 22.25 21H1.75A1.75 1.75 0 0 1 0 19.25V4.75C0 3.784.784 3 1.75 3Zm-.25 7v9.25c0 .138.112.25.25.25h20.5a.25.25 0 0 0 .25-.25V10Zm0-5.25V8.5h21V4.75a.25.25 0 0 0-.25-.25H1.75a.25.25 0 0 0-.25.25Z"></path></svg>Asset</strong>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,18 @@
 	import { get } from 'svelte/store';
 	import { localStorageStore } from '@skeletonlabs/skeleton';
 	import { getToastStore } from '@skeletonlabs/skeleton';
+	import { clipboard } from '@skeletonlabs/skeleton';
+	import { fade } from 'svelte/transition';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+
+	let showThumbsUp = false;
+
+	function handleCopyButtonClick() {
+		showThumbsUp = true;
+		setTimeout(() => {
+		showThumbsUp = false;
+		}, 1500);
+	}
 
     const toastStore = getToastStore();
     let fundsStore = localStorageStore('funds', []); // initialize with an empty array
@@ -39,7 +51,8 @@
 	$: portfolioSum = funds.reduce((total, fund) => total + Number(fund.amount), 0) + toAdd;
 
 	let advice = [];
-	let adviceString = '';
+	$: adviceString = advice.join(' ').replace(/<\/?[^>]+(>|$)/g, "");
+
 	function addFund() {
 		const newFund = { id: funds.length + 1, name: '', amount: '', desired_perc: '' };
 		funds = [...funds, newFund];
@@ -53,24 +66,34 @@
 		advice = [];
 		// Check if all of the "desired_perc" add up to 100%
 		const totalPercentage = funds.reduce((total, fund) => total + Number(fund.desired_perc), 0);
-		console.log('Total Percentage:', totalPercentage);
 		if (totalPercentage !== 100) {
 			toastStore.trigger({message: 'The desired percentages do not add up to 100%.', 	background: 'variant-filled-error',})
 			return;
 		}
 
-		// 1. Calculate new total portfolio value.
-		advice.push('Portfolio Sum:', portfolioSum);
+		// Check if the total portfolio value is greater than the cash outflow.
+		if (toAdd >= portfolioSum) {
+			toastStore.trigger({message: 'The cash outflow is greater than the total portfolio value.', background: 'variant-filled-error',})
+			return;
+		}
 
-		// 2. Calculate desired value per fund including new cash flow and calculate diff. between current and desired values.
+		// Check if any fields are empty
+		const emptyFieldFund = funds.find(fund => !fund.id || !fund.name || !fund.amount || !fund.desired_perc);
+		if (emptyFieldFund) {
+			const emptyFields = Object.entries(emptyFieldFund).filter(([key, value]) => !value).map(([key]) => key);
+			toastStore.trigger({message: `The following fields are missing: ${emptyFields.join(', ')}`, background: 'variant-filled-error',})
+			return;
+		}
+		
+
+		// 1. Calculate desired value per fund including new cash flow and calculate diff. between current and desired values.
 		const fundChanges = funds.map(fund => {
 			const desiredAmount = (portfolioSum * fund.desired_perc) / 100;
 			const difference = desiredAmount - fund.amount;
-			advice.push('Fund:', fund.name, 'Desired Amount:', desiredAmount, 'Difference:', difference);
 			return { ...fund, desiredAmount, difference };
 		});
 
-		// 3. Split funds into two arrays: one for funds where you need to add, and one for funds where you need to remove.
+		// 2. Split funds into two arrays: one for funds where you need to add, and one for funds where you need to remove.
 		let toAddFunds = fundChanges.filter(fund => fund.difference > 0);
 		let toRemoveFunds = fundChanges.filter(fund => fund.difference < 0);
 
@@ -80,12 +103,8 @@
 		// 5. For funds where you need to remove, sort them in ascending order of difference. Start removing from the fund with the lowest difference. This also minimizes the number of transactions.
 		toRemoveFunds.sort((a, b) => a.difference - b.difference);
 
-		console.log('To Add Funds:', toAddFunds);
-		console.log('To Remove Funds:', toRemoveFunds);
-
 		// 6. Keep track of the remaining cash inflow. When adding to a fund, subtract the added amount from the remaining cash inflow. When removing from a fund, add the removed amount to the remaining cash inflow.
 		let remainingCashInflow = toAdd;
-		console.log('Initial Remaining Cash Inflow:', remainingCashInflow);
 
 		// 8. Repeat steps 5-7 until the remaining cash inflow is zero or close enough.
 		while (toAddFunds.some(fund => fund.difference > 0.01) || toRemoveFunds.some(fund => fund.difference < -0.01)) {
@@ -95,21 +114,20 @@
 				fund.amount += addAmount;
 				remainingCashInflow -= addAmount;
 				fund.difference = fund.desiredAmount - fund.amount;
-				advice.push('Add:', addAmount, 'to', fund.name, '. Remaining Cash Inflow:', remainingCashInflow);
-				}
+				advice.push(`Add <span class="font-semibold p-1 bg-success-500 text-white rounded-lg">${addAmount}</span> to ${fund.name}. `);				}
 			});
 
 			toRemoveFunds.forEach(fund => {
-				if (remainingCashInflow > 0.01) {
-				const removeAmount = Math.min(-fund.difference, remainingCashInflow);
-				fund.amount -= removeAmount;
-				remainingCashInflow += removeAmount;
-				fund.difference = fund.desiredAmount - fund.amount;
-				advice.push('Remove:', removeAmount, 'from', fund.name, '. Remaining Cash Inflow:', remainingCashInflow);
+				if (remainingCashInflow < -0.01) {
+					const removeAmount = Math.min(-fund.difference, -remainingCashInflow);
+					fund.amount -= removeAmount;
+					remainingCashInflow += removeAmount;
+					fund.difference = fund.desiredAmount - fund.amount;
+					advice.push(`Remove <span class="font-semibold p-1 text-white rounded-lg bg-warning-500">${removeAmount}</span> from ${fund.name}. `);
 				}
 			});
 
-			if (remainingCashInflow <= 0.01) {
+			if (Math.abs(remainingCashInflow) <= 0.01) {
 				const fundToRemoveFrom = toRemoveFunds.find(fund => fund.difference < -0.01);
 				const fundToAddTo = toAddFunds.find(fund => fund.difference > 0.01);
 				if (fundToRemoveFrom && fundToAddTo) {
@@ -118,16 +136,10 @@
 				fundToAddTo.amount += transferAmount;
 				fundToRemoveFrom.difference = fundToRemoveFrom.desiredAmount - fundToRemoveFrom.amount;
 				fundToAddTo.difference = fundToAddTo.desiredAmount - fundToAddTo.amount;
-				advice.push('Transfer:', transferAmount, 'from', fundToRemoveFrom.name, 'to', fundToAddTo.name);
-				}
+				advice.push(`Transfer <span class="font-semibold p-1 bg-neutral-500 rounded-lg text-white">${transferAmount}</span> from ${fundToRemoveFrom.name} to ${fundToAddTo.name}. `);				}
 			}
 
-			console.log('Remaining Cash Inflow:', remainingCashInflow);
-			console.log('To Add Funds:', toAddFunds);
-			console.log('To Remove Funds:', toRemoveFunds);
-
 			generated = true;
-			adviceString = advice.join('\n\n');
 		}
 		}
 
@@ -138,24 +150,43 @@
 		$: totalPercentage = funds.reduce((total, fund) => total + fund.desired_perc, 0);
 
 </script>
-  <div class="container h-full mx-auto flex justify-center pb-40 items-center">
+  <div class="container h-full mx-auto flex flex-col flex-nowrap justify-center pt-10 pb-40 items-center">
 	<!-- Responsive Container (recommended) -->
 	{#if loading}
 		<div class="flex justify-center items-center">
-			<div class="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-secondary-500 shadow-black shadow-sm"></div>
+			<div class="animate-spin rounded-full h-8 w-8 border-t-4 border-b-4 border-secondary-500 shadow-black shadow-sm"></div>
 		</div>
 	{:else}
+	<div class="bg-surface-50-900-token !text-surface-900-50-token w-full">
+		<Accordion autocollapse>
+			<AccordionItem class="">
+				<svelte:fragment slot="lead"></svelte:fragment>
+				<svelte:fragment slot="summary">WTF is this?</svelte:fragment>
+				<svelte:fragment slot="content">Hi</svelte:fragment>
+			</AccordionItem>
+			<AccordionItem class="">
+				<svelte:fragment slot="lead"></svelte:fragment>
+				<svelte:fragment slot="summary">How do I use it?</svelte:fragment>
+				<svelte:fragment slot="content">Hi</svelte:fragment>
+			</AccordionItem>
+			<AccordionItem class="">
+				<svelte:fragment slot="lead"></svelte:fragment>
+				<svelte:fragment slot="summary">What do I use it for?</svelte:fragment>
+				<svelte:fragment slot="content">Hi</svelte:fragment>
+			</AccordionItem>
+		</Accordion>
+	</div>
 	<div class="table-container">
 		<!-- Native Table Element -->
-		<table class="table table-hover">
+		<table class="table table-interactive">
 			<thead>
-				<tr class="text-lg">
-					<th class="text-center">Funds</th>
-					<th class="text-center">Amount</th>
-					<th class="text-center transition-colors duration-300 rounded-lg" class:bg-warning-700={totalPercentage !== 100}>Target percentage</th>
+				<tr class="text-lg bg-secondary-400 text-white">
+					<th class="text-center">Assets</th>
+					<th class="text-center">Current Value</th>
+					<th class="text-center transition-colors duration-300 rounded-lg">Target allocation (%)</th>
 					<th class="text-center">
 						<button on:click={addFund}>
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-1">
 								<path d="M6 3a3 3 0 0 0-3 3v2.25a3 3 0 0 0 3 3h2.25a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3H6ZM15.75 3a3 3 0 0 0-3 3v2.25a3 3 0 0 0 3 3H18a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3h-2.25ZM6 12.75a3 3 0 0 0-3 3V18a3 3 0 0 0 3 3h2.25a3 3 0 0 0 3-3v-2.25a3 3 0 0 0-3-3H6ZM17.625 13.5a.75.75 0 0 0-1.5 0v2.625H13.5a.75.75 0 0 0 0 1.5h2.625v2.625a.75.75 0 0 0 1.5 0v-2.625h2.625a.75.75 0 0 0 0-1.5h-2.625V13.5Z" />
 							</svg>
 							Add fund</button>
@@ -187,34 +218,58 @@
 									<path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
 								</svg>
 							</button>
-							<div class="card p-4 variant-filled-secondary" data-popup="removePopup-{i+1}">
-							  <span class="text-base">Are you sure?</span>
+							<div class="card p-4 bg-secondary-500 text-white" data-popup="removePopup-{i+1}">
+							  <span class="text-base text-white">Are you sure?</span>
 							  <button on:click={() => removeFund(fund.id)} class="ml-1">Remove</button>
 							  <div class="arrow bg-secondary-500" />
 						  	</div>
 						</td>
 					</tr>
 				{/each}
+					<tr>
+						<th colspan="1" class="cursor-auto">New capital</th>
+						<td on:click={() => focusInput(`to_add`)} class="cursor-text">
+							<input id="to_add" bind:value={toAdd} class="w-ful text-center" placeholder="Portfolio amount" type="number" />
+						</td>
+						<td on:click={() => focusInput(`to_add`)} class="cursor-text"></td>
+						<td on:click={() => focusInput(`to_add`)} class="cursor-text"></td>
+					</tr>
 			</tbody>
 			<tfoot>
-				<tr>
-					<th colspan="1">New cash flow</th>
-					<td on:click={() => focusInput(`to_add`)} class="cursor-text">
-						<input id="to_add" bind:value={toAdd} class="w-ful text-center" placeholder="Portfolio amount" type="number" />
-					</td>
-				</tr>
-				<tr>
-					<th colspan="1">New portfolio total</th>
-					<td class="text-center relative before:block before:absolute before:top-0 before:h-[0.125rem] before:rounded-full before:w-1/3 before:left-1/2 before:-translate-x-1/2 before:bg-surface-900-50-token">
+				<tr class="bg-secondary-400 text-white" >
+					<th colspan="1">New Portfolio Total</th>
+					<td class="text-center relative">
 						{portfolioSum}</td>
-					<td>
-						<button on:click={generate}>Generate</button>
+					<td colspan="2" class="">
+						<button class="ml-48" on:click={generate}>Generate</button>
 					</td>
 				</tr>
 				{#if generated}
 					<tr>
-						<td colspan="4">
-							{adviceString}
+						<td colspan="4" >
+							<span class="font-bold pb-4">Follow these steps to rebalance your portfolio:
+							</span>
+							<ol class="list pt-4">
+								{#each advice as item, i}
+										<li>
+											<span id="numberSpan">{i+1}</span>
+											<span class="flex-auto">{@html item}</span>
+										</li>
+										<!-- ... -->
+								{/each}
+							</ol>
+							<div class="flex items-center gap-x-2 pt-4">
+								<button class="btn variant-filled" on:click={handleCopyButtonClick} use:clipboard={adviceString}>
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-1">
+										<path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clip-rule="evenodd" />
+										<path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+									</svg>
+									Copy
+								</button>
+								{#if showThumbsUp}
+									<span class="text-xl bg-surface-200 rounded-full p-1" out:fade={{ duration: 250 }}>&#x1F44D;</span>
+								{/if}
+							</div>
 						</td>
 					</tr>
 				{/if}
